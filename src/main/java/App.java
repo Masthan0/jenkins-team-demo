e94d909b53e91ee774492ab3627bd92a975829e1
Masthan0 <masthanshaik9712@gmail.com>
2026-04-17 13:24:02 -0500

    Updated App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Team Project. First change!..");
+	System.out.println("Succesfully launching into AWS");
     }
 }
