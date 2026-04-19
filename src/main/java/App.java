064319206c772de3a38e7d565b667fc97a506951
Masthan0 <masthanshaik9712@gmail.com>
2026-04-19 15:12:14 -0500

    AWS automation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,5 +4,6 @@ public class App {
 	System.out.println("Successfully launching into AWS.");
 	System.out.println("Successfully automated docker images on to EC2 server...........Finallyyyyyyy..............Hurahhhhhhh!");
 	System.out.println("Hello From AWS EC2");
+	System.out.println("Testing it again.................................................................");
     }
 }
